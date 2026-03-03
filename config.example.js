e8df0e4712529ec7e06b15e65f5982bce536ddfd
Resonance-coder <youthconnect.lin@gmail.com>
2026-03-03 22:13:10 +0700

    Update gasUrl in config.example.js

--- config.example.js
+++ config.example.js
@@ -1,3 +1,4 @@
 window.__FORM_CONFIG__ = {
-  gasUrl: "https://script.google.com/macros/s/REPLACE_WITH_YOUR_DEPLOYMENT_ID/exec"
+  gasUrl: "https://script.google.com/macros/s/AKfycbyrqFTPNwHQddpsEl--w7voYus_ge47_WdXfBNj-Gd-asMcPiCjSbKlSCGHTouMdchK/exec"
 };
+
